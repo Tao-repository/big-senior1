6f76bfec6ea8dea72b93d0c3d8c297e70b128e13
Tao-repository <2639307585@qq.com>
2020-03-15 21:36:35 +0800

    fix page

--- jcart-administration-front/route-page/router.js
+++ jcart-administration-front/route-page/router.js
@@ -9,8 +9,31 @@ const routes = [
     { path: '/product/update/:productId', component: ProductUpdateRoutePage },
 
     { path: '/customer/search', component: CustomerSearchRoutePage },
+    { path: '/customer/show/:customerId', component: CustomerShowRoutePage },
+    { path: '/address/index/:customerId', component: AddressIndexRoutePage },
+    { path: '/address/show/:addressId', component: AddressShowRoutePage },
+
+
     { path: '/order/search', component: OrderSearchRoutePage },
-    { path: '/return/search', component: ReturnSearchRoutePage }
+    { path: '/order/show/:orderId', component: OrderShowRoutePage },
+
+    { path: '/return/search', component: ReturnSearchRoutePage },
+    {
+        path: '/return/edit/:returnId',
+        component: ReturnEditRoutePage,
+        children: [
+            {
+                path: 'show',
+                component: ReturnShowRoutePage
+            },
+            {
+                path: 'history',
+                component: ReturnHistoryIndexRoutePage
+            }
+        ]
+    }
+
+
 
 ];
 
